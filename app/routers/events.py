07c41b81193b1c7705625f9cf5a67b97657c9897
gianlucazucca03-cmyk <gianlucazucca03@libero.it>
2026-06-27 20:38:25 +0200

    Fix event registration

--- app/routers/events.py
+++ app/routers/events.py
@@ -142,10 +142,10 @@ def register_to_event(event_id: int, user_data: UserCreate, session: SessionDep)
     Se l'utente non esiste, viene creato automaticamente.
     """
     event = session.get(Event, event_id)
-    #controllo evento
+
     if event is None:
         raise HTTPException(status_code=404, detail="Event not found")
-        
+
     now = datetime.now()
 
     if now > event.end_late_reg:
@@ -153,68 +153,53 @@ def register_to_event(event_id: int, user_data: UserCreate, session: SessionDep)
             status_code=400,
             detail="Registrazione tardiva terminata",
         )
-    
-      
-    existing_user = session.get(User, user_data.username)
-    
-    #controllo user
-    if existing_user is not None:
-        raise HTTPException(
-            status_code=400,
-            detail="User already exists"
-        )
 
-    #controllo email
-    existing_email = session.exec(
-        select(User).where(User.email == user_data.email)
-    ).first()
-
-    if existing_email is not None  and existing_email.username != user_data.username:
-        raise HTTPException(
-            status_code=400,
-            detail="Email already exists, no multi-account"
-        )
-
-    #controllo carta
-    existing_card = session.exec(
-        select(User).where(User.numero_carta == user_data.numero_carta)
-    ).first()
-    
-    if existing_card is not None and existing_card.username != user_data.username:
-        raise HTTPException(
-            status_code=400,
-            detail="Card already exists"
-        )
-    raise HTTPException(status_code=400, detail="Card already exists, no multi-account")
-    
-    #controllo vincoli carte e cvv
     validate_card(user_data.numero_carta, user_data.cvv_carta)
-    #controllo nome vincolo
     validate_name(user_data.name)
 
     user = session.get(User, user_data.username)
-    
-    
+
     if user is None:
+        existing_email = session.exec(
+            select(User).where(User.email == user_data.email)
+        ).first()
+
+        if existing_email is not None:
+            raise HTTPException(
+                status_code=400,
+                detail="Email already exists, no multi-account",
+            )
+
+        existing_card = session.exec(
+            select(User).where(User.numero_carta == user_data.numero_carta)
+        ).first()
+
+        if existing_card is not None:
+            raise HTTPException(
+                status_code=400,
+                detail="Card already exists, no multi-account",
+            )
+
         user = User(
             username=user_data.username,
             name=user_data.name,
             email=user_data.email,
-            numero_carta=user_data.numero_carta,    #togliere per progetto realistico, tenere per la richiesta del prof sul API
-            cvv_carta=user_data.cvv_carta,          #togliere per progetto realistico, tenere per la richiesta del prof sul API
+            numero_carta=user_data.numero_carta,
+            cvv_carta=user_data.cvv_carta,
         )
+
         session.add(user)
         session.commit()
         session.refresh(user)
 
     existing_registration = session.get(
         Registration,
-        (user_data.username, event_id),
+        (user.username, event_id),
     )
 
     if existing_registration is not None:
         return existing_registration
-    
+
     if user.saldo < event.buy_in:
         raise HTTPException(
             status_code=400,
@@ -222,12 +207,13 @@ def register_to_event(event_id: int, user_data: UserCreate, session: SessionDep)
         )
 
     user.saldo -= event.buy_in
+
     session.add(user)
     session.commit()
     session.refresh(user)
-    
+
     registration = Registration(
-        username=user_data.username,
+        username=user.username,
         event_id=event_id,
     )
 
